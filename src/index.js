20c38251de456af892a3211f43abe91dc7fe54ff
asnouski <osnovvv@gmail.com>
2021-03-09 22:28:57 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+	let array = [];
+	let recursia = (arr) => {
+		if (!arr || arr.length === 0) return [];
+		for (var i = 0; i < arr.length; i++) {
+			if (typeof arr[i] !== 'object') array.push(arr[i]);
+			if (typeof arr[i] === 'object') {
+				if (i % 2 !== 0) recursia(arr[i].reverse());
+				else recursia(arr[i]);
+			}
+		}
+		return array;
+	}
+	return recursia(matrix);
 }
